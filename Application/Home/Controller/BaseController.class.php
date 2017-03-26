cda164f594f34b12724535ac1f02835dd09a00cd
Lich2013 <longzy2013@gmail.com>
2017-03-26 20:04:58 +0800

    基本ok

--- Application/Home/Controller/BaseController.class.php
+++ Application/Home/Controller/BaseController.class.php
@@ -7,5 +7,47 @@ class BaseController extends Controller {
     public function _initialize() {
         session('nickname', 'test');
         session('openid', 'asdf');
+        header('Access-Control-Allow-Origin: *');
+        $openid = 'asdf';//session('openid');//'ouRCyjhdsj8RQofIOPHc7nX9hA98';//todo
+        $nickname = 'test';//session('nickname');
+        if (!$openid || !$nickname) {
+            $openid = I('get.openid');
+            $nickname = urldecode(I('get.nickname'));//'知识混子周政';//
+        }
+        if (!$openid || !$nickname) {
+            $uri = 'http://hongyan.cqupt.edu.cn/MagicLoop/index.php?s=/addon/Api/Api/oauth&redirect='.urlencode('https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+            redirect($uri);
+        }
+        session('openid', $openid);
+        session('nickname', $nickname);
+        $users = M('users');
+        $num = $users->where(array('openid' => $openid))->count();
+        if ($num == 0) {
+            $data = array(
+                'openid' => $openid,
+                'nickname' => $nickname,
+                'days'   => 0,
+                'count'  => 0,
+                'imgurl' => urldecode(I('get.headimgurl')),
+                'score'  => 0
+            );
+            $users->add($data);
+            $userCurrent = M('user_current_question');
+            $currentData = array(
+                'openid' => $openid,
+                'current' => 0,
+                'today_group_count' => 0,
+                'today_learn_id' => json_encode(array()),
+                'date' => date('Y-m-d', time()),
+            );
+            $userCurrent->add($currentData);
+        } else {
+            $img = I('get.headimgurl');
+            if ($nickname && $img) {
+                $data['nickname'] = $nickname;
+                $data['imgurl'] = urldecode($img);
+                $users->where(array('openid' => $openid))->save($data);
+            }
+        }
     }
 }
--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,15 +1,15 @@
 <?php
 namespace Home\Controller;
-
+use Org\Util\String;
 use Think\Controller;
 
 class IndexController extends BaseController {
     private $total = 5;
-    private $chooseCount = 2;
+    private $chooseCount = 3;
+    private $appid = 'wx81a4a4b77ec98ff4';
+    private $acess_token = 'gh_68f0a1ffc303';
     public function index() {
-        $data = '一枝理执思服发于法';
-        $data = preg_split('/(?<!^)(?!$)/u', $data );
-        var_dump($data);
+
     }
 
     public function question() {
@@ -38,9 +38,9 @@ class IndexController extends BaseController {
 
         $currentLearn = json_decode($user['today_learn_id']);
         if ($user['current'] < $this->chooseCount) {
-            $data = $this->choose($currentLearn);
+            $data = $this->fillblank($currentLearn, $user['current']);
         } elseif ($user['current'] >= $this->chooseCount && $user['current'] < $this->total){
-            $data = $this->fillblank($currentLearn);
+            $data = $this->choose($currentLearn);
         } else {
             $this->ajaxReturn(array(
                 'status' => 500,
@@ -60,6 +60,37 @@ class IndexController extends BaseController {
         $this->ajaxReturn(array(
             'status' => 200,
             'data'  => $data
+        ));
+    }
+
+    public function rank() {
+        $users = M('users');
+        $openid = session('openid');
+        $user = $users->where(array('openid' => $openid))->find();
+        $map['count'] = array('GT', $user['count']);
+        $rank = $users->where($map)->count();
+        $rank += 1;
+        $list = $users->order('count desc')->field('nickname, imgurl as avatar')->limit(10)->select();
+        if ($rank <= 50) {
+            $real = $users->order('count desc')->field('nickname, imgurl')->limit(50)->select();
+        }
+        foreach ($real as $key => $value) {
+            if ($value['nickname'] == $user['nickname']) {
+                $rank = $key+1;
+            }
+        }
+        if ($user['count'] == 0) {
+            $rank = '∞';
+        }
+        $this->ajaxReturn(array(
+            'status' => 200,
+            'data'   => array(
+                'list' => $list,
+                'rank' => $rank,
+                'nickname' => $user['nickname'],
+                'avatar' => $user['imgurl'],
+                'count' => $user['count'],
+            )
         ));
     }
 
@@ -84,7 +115,13 @@ class IndexController extends BaseController {
         return $data;
     }
 
-    private function fillblank(&$currentData) {
+    private function fillblank(&$currentData, $current) {
+        if ($current == 0) {
+            $map['special_type'] = 'congyanzhidang';
+        }
+        if ($current == 1) {
+            $map['special_type'] = 'qingniangongzuo';
+        }
         if ($currentData->fillblank) {
             $map['id'] = array('NOT IN', $currentData->fillblank);
             $question = M('fillblank')->where($map)->order('[RAND]')->find();
@@ -94,10 +131,12 @@ class IndexController extends BaseController {
         array_push($currentData->fillblank, $question['id']);
         $options = preg_split('/(?<!^)(?!$)/u', $question['answer']);
         $num = 8 - count($options);
-        $cmap['chracter'] =  array('NOT IN', $options);
-        $add = M('chracters')->where($cmap)->order('[RAND]')->limit($num)->field('chracter')->select();
-        foreach ($add as $v) {
-            $options = array_merge($options, array($v['chracter']));
+        if ($num != 0) {
+            $cmap['chracter'] =  array('NOT IN', $options);
+            $add = M('chracters')->where($cmap)->order('[RAND]')->limit($num)->field('chracter')->select();
+            foreach ($add as $v) {
+                $options = array_merge($options, array($v['chracter']));
+            }
         }
         shuffle($options);
         $data = array(
@@ -111,5 +150,62 @@ class IndexController extends BaseController {
             $data['image'] = rand(1, 23).'.png';
         }
         return $data;
+    }
+
+    public function JSSDKSignature(){
+        $string = new String();
+        $jsapi_ticket =  $this->getTicket();
+        $data['jsapi_ticket'] = $jsapi_ticket['data'];
+        $data['noncestr'] = $string->randString();
+        $data['timestamp'] = time();
+        $data['url'] = 'https://'.$_SERVER['HTTP_HOST'].__SELF__;//生成当前页面url
+        $data['signature'] = sha1($this->ToUrlParams($data));
+        return $data;
+    }
+    private function ToUrlParams($urlObj){
+        $buff = "";
+        foreach ($urlObj as $k => $v) {
+            if($k != "signature") {
+                $buff .= $k . "=" . $v . "&";
+            }
+        }
+        $buff = trim($buff, "&");
+        return $buff;
+    }
+
+
+    /*curl通用函数*/
+    private function curl_api($url, $data=''){
+        // 初始化一个curl对象
+        $ch = curl_init();
+        curl_setopt ( $ch, CURLOPT_URL, $url );
+        curl_setopt ( $ch, CURLOPT_POST, 1 );
+        curl_setopt ( $ch, CURLOPT_HEADER, 0 );
+        curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
+        curl_setopt ( $ch, CURLOPT_POSTFIELDS, $data );
+        // 运行curl，获取网页。
+        $contents = json_decode(curl_exec($ch), true);
+        // 关闭请求
+        curl_close($ch);
+        return $contents;
+    }
+
+    private function getTicket() {
+        $time = time();
+        $str = 'abcdefghijklnmopqrstwvuxyz1234567890ABCDEFGHIJKLNMOPQRSTWVUXYZ';
+        $string='';
+        for($i=0;$i<16;$i++){
+            $num = mt_rand(0,61);
+            $string .= $str[$num];
+        }
+        $secret =sha1(sha1($time).md5($string)."redrock");
+        $t2 = array(
+            'timestamp'=>$time,
+            'string'=>$string,
+            'secret'=>$secret,
+            'token'=>$this->acess_token,
+        );
+        $url = "http://hongyan.cqupt.edu.cn/MagicLoop/index.php?s=/addon/Api/Api/apiJsTicket";
+        return $this->curl_api($url, $t2);
     }
 }
